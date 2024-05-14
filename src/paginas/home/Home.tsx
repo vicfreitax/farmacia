7fe7abe95908321a1489505a7f32f00414631563
vicfreitax <victoriafreitax@gmail.com>
2024-05-14 01:28:37 -0300

    primeiro commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Inicial</h1>
+            <img src="https://i.imgur.com/peH6IMo.jpeg" alt="Imagem Tela Inicial" className="img"/>
+           
+        </>
+    );
+}
+
+export default Home;
